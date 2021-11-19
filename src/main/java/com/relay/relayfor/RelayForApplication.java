903f673f8745f39a6a4bb6796e25d9072066f0fe
Anthony Casper <anthony.casper@galvanize.com>
2021-11-19 11:47:26 -0600

    Initial Spring Boot server setup

---
+++ src/main/java/com/relay/relayfor/RelayForApplication.java
@@ -0,0 +1,13 @@
+package com.relay.relayfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayForApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayForApplication.class, args);
+	}
+
+}
